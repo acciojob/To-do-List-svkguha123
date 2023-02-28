691e6f42394769b3b3bac998b24e262209b3b556
Souvik Guha <svkguha123@gmail.com>
2023-02-28 12:45:12 +0530

    ff

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //your code here
+function addTodo() {
+    const todoText = newTodoInput.value.trim();
+    if (todoText !== "") {
+      const todoItem = document.createElement("li");
+      todoItem.innerText = todoText;
+      todoList.appendChild(todoItem);
+      newTodoInput.value = "";
+    }
+  }
+  
+  addTodoBtn.addEventListener("click", addTodo);
 
